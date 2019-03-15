d7a60be97d17203bb7f830deddf665aa6b3b6485
derrickhamilton <derrickhamilton212@gmail.com>
2019-03-15 14:21:17 -0400

    set up event handlers and created the model class

--- CalculatorMVC/ViewController.swift
+++ CalculatorMVC/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var outLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func digitBtn(_ sender: UIButton) {
+    }
+    @IBAction func binOpBtn(_ sender: UIButton) {
+    }
+    @IBAction func equalsBtn(_ sender: UIButton) {
+    }
+    @IBAction func clearBtn(_ sender: UIButton) {
+    }
 }
-
---
+++ CalculatorMVC/calcModel.swift
@@ -0,0 +1,12 @@
+//
+//  calcModel.swift
+//  CalculatorMVC
+//
+//  Created by Derrick J. Hamilton on 3/15/19.
+//  Copyright © 2019 Derrick J. Hamilton. All rights reserved.
+//
+
+import Foundation
+
+class calcModel {
+}
